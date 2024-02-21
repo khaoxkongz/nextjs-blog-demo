50671ae0256c2f5c2a74ef8203784e0ec608327a
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-21 14:05:53 +0700

    refactor: change middle ware to nothing

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     console.log("test");
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
