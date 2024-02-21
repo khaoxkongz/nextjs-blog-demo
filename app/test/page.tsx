37b584486d15159254a75fc769da152029b0d96c
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-21 14:29:53 +0700

    feat: fetch data via API in client component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,10 @@
-export default function Page() {
-    return (
-        <div>
-            Test Page
-        </div>
-    )
+"use client";
+
+import { getBlogs } from "../content/page";
+
+export default async function Page() {
+    const blogs = await getBlogs();
+    console.log("blog", blogs)
+
+    return <div>Test Page 2</div>;
 }
